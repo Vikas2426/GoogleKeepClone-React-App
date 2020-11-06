c3850691c42c83adeaa30cbca65e5c9a650ea87b
Vikas Choubey <vikas.cby@gmail.com>
2020-11-06 21:05:43 +0530

    Updates

---
+++ precache-manifest.eca9b2197ca020ab4ccedf6a733377cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7e336d7ba395dd60a51642f92813ae0",
+    "url": "/GoogleKeepClone-React-App/index.html"
+  },
+  {
+    "revision": "4ca0d22e390c4689be8c",
+    "url": "/GoogleKeepClone-React-App/static/css/main.20102911.chunk.css"
+  },
+  {
+    "revision": "c2ed34249838ab96151a",
+    "url": "/GoogleKeepClone-React-App/static/js/2.c7919bba.chunk.js"
+  },
+  {
+    "revision": "1260a01290ac0c0454e9a7627323041c",
+    "url": "/GoogleKeepClone-React-App/static/js/2.c7919bba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ca0d22e390c4689be8c",
+    "url": "/GoogleKeepClone-React-App/static/js/main.4a158f48.chunk.js"
+  },
+  {
+    "revision": "776c8b0b734817c9effb",
+    "url": "/GoogleKeepClone-React-App/static/js/runtime-main.605877b8.js"
+  }
+]);
