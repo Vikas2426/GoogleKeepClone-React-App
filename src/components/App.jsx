7b8d5e50953225f10fa290ddc94f6fb15a80ae23
Vikas2426 <vikas.cby@gmail.com>
2021-03-17 21:05:47 +0530

    fix: lint errors

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,30 @@
-
-import React, { useState } from "react";
-import Header from "./Header";
-import CreateArea from "./CreateArea";
+/* eslint-disable import/extensions */
+import React, { useState } from 'react';
+import Header from './Header';
+import CreateArea from './CreateArea';
 import './styles.css';
 import notes from '../notes.js';
 import NotesList from './NotesList.jsx';
 
 function App() {
   const [notesList, setNotesList] = useState(notes);
-  const [searchItem, setSearchItem] = useState("");
+  const [searchItem, setSearchItem] = useState('');
 
   const addNote = (newTitle, newContent) => {
     const newNote = {
       title: newTitle,
-      content: newContent
+      content: newContent,
     };
 
-    setNotesList(prevValues => [...prevValues, newNote]);
+    setNotesList((prevValues) => [...prevValues, newNote]);
   };
 
-  const deleteNote = id => {
-    setNotesList(prevValues =>
-      prevValues.filter((note, index) => index !== id)
-    );
+  const deleteNote = (id) => {
+    setNotesList((prevValues) => prevValues.filter((note, index) => index !== id));
   };
-  const search = searchText => {
+  const search = (searchText) => {
     setSearchItem(searchText);
-  }
+  };
 
   return (
     <div className="app">
@@ -34,9 +32,12 @@ function App() {
       <CreateArea createNote={addNote} />
       {/* eslint-disable-next-line  */}
       <div className='app-body'>
-        <NotesList notes={notesList.filter((note) =>
-          (note.title.toLowerCase().includes(searchItem) || note.content.toLowerCase().includes(searchItem)))
-        } deleteNote={deleteNote} />
+        <NotesList
+          notes={notesList.filter((note) => (
+            // eslint-disable-next-line
+            note.title.toLowerCase().includes(searchItem) || note.content.toLowerCase().includes(searchItem)))}
+          deleteNote={deleteNote}
+        />
       </div>
       {/* <Footer className='footer' /> */}
     </div>
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,19 +1,20 @@
-import React from "react";
-import DeleteIcon from "@material-ui/icons/Delete";
+import React from 'react';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 function Note(props) {
-  const handleClick = event => {
+  const handleClick = (event) => {
     props.delete(props.id);
   };
   return (
     <div className="note">
       <h1>{props.title}</h1>
-      <p>{props.content}
-        <span id='delete-btn' >
-          {<DeleteIcon
+      <p>
+        {props.content}
+        <span id="delete-btn">
+          <DeleteIcon
             onClick={handleClick}
-            id='delete-btn'
-          />}
+            id="delete-btn"
+          />
         </span>
       </p>
     </div>
